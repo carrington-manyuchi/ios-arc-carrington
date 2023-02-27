09104ab691878cd211a45704592b6deaeb14cf1b
Carrington <Manyuchi>
2023-02-27 19:51:40 +0200

    testing json files

--- ios-arc-carrington/ViewController.swift
+++ ios-arc-carrington/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     
     func ReadJSON() {
         
-        let fileName = "test2"
+        let fileName = "test1"
         let fileType = "json"
         
         if  let path = Bundle.main.path(forResource: fileName, ofType: fileType) {
@@ -30,11 +30,15 @@ class ViewController: UIViewController {
             let data = try Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
                 
                 let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-                
+
                 print(jsonObject)
                 
-            } catch {
+                //let testOne = try? JSONDecoder().decode(TestOne.self, from: data)
                 
+            
+                
+            } catch {
+                print("JSON file was not found")
             }
             
         } else {
@@ -44,23 +48,8 @@ class ViewController: UIViewController {
         
     }
     
-//    func ReadingJSONFile() {
-//
-//        let fileName = "test1"
-//        let fileType = "json"
-//
-//        if let path = Bundle.main.path(forResource: fileName, ofType: fileType) {
-//            // fileURLWithPath
-//        do {
-//            let data = try Data(contentsOf: URL(filePath: path), options: .mappedIfSafe)
-//            let jsonObject = try JSONSerialization.jsonObject(with: data, options: .mutableLeaves)
-//            print(jsonObject)
-//
-//        } catch {
-//            print("JSON file not found")
-//        }
-//      }
-//    }
 
 }
+
+
 
